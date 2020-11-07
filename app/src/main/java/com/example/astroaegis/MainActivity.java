e0dbb5ff119c25c160819917a9365d835a2acb84
navneeeth <navneeth.padaki15@gmail.com>
2020-11-07 23:06:11 +0530

    Initial groundwork.

--- app/src/main/java/com/example/astroaegis/MainActivity.java
+++ app/src/main/java/com/example/astroaegis/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.astroaegis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button newHoroscopeButton, openHoroscopeButton, worldChartButton;
+        Intent newHoroscopeActivityIntent, openHoroscopeActivityIntent, worldChartButtonActivityIntent;
+        newHoroscopeButton = findViewById(R.id.newHoroscopeButton);
+        openHoroscopeButton = findViewById(R.id.openHoroscopeButton);
+        worldChartButton = findViewById(R.id.worldChartButton);
+        newHoroscopeActivityIntent = new Intent(MainActivity.this, NewHoroscopeActivity.class);
+        newHoroscopeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(newHoroscopeActivityIntent);
+            }
+        });
+        openHoroscopeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Feature still being developed!", Toast.LENGTH_LONG);
+            }
+        });
+        worldChartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Feature still being developed!", Toast.LENGTH_LONG);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/astroaegis/NewHoroscopeActivity.java
@@ -0,0 +1,14 @@
+package com.example.astroaegis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewHoroscopeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_horoscope);
+    }
+}
