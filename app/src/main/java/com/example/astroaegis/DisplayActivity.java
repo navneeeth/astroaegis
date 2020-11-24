15d0cdd9949abce834f6520a0bedea6293617ad4
navneeeth <navneeth.padaki15@gmail.com>
2020-11-24 21:50:50 +0530

    Building a DisplayActivity.

--- app/src/main/java/com/example/astroaegis/DisplayActivity.java
+++ app/src/main/java/com/example/astroaegis/DisplayActivity.java
@@ -31,7 +31,7 @@ public class DisplayActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your ow", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
